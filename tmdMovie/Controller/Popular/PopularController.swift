85787f9df1984b086ea2c1850b517cfe583a9f82
memo figueredo <memof80@gmail.com>
2021-09-14 13:12:44 -0500

    pass data to controllers in PopularController

--- tmdMovie/Controller/Popular/PopularController.swift
+++ tmdMovie/Controller/Popular/PopularController.swift
@@ -13,6 +13,19 @@ class PopularViewController: BaseListController, UICollectionViewDelegateFlowLay
 //    MARK:- Identifier Cell
     fileprivate let cellId = "id1234"
     
+//    MARK: - DataBaseLocal
+    let database = DatabaseHandler.shared
+    
+//    MARK: - Reload collectionView
+    var movies : [Movies]? {
+        didSet {
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+      
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .green
@@ -20,15 +33,32 @@ class PopularViewController: BaseListController, UICollectionViewDelegateFlowLay
         collectionView.register(PopulaViewCell.self, forCellWithReuseIdentifier: cellId)
     }
     
+//    MARK: Save Data to Api to Core Data Model
+    override func viewDidAppear(_ animated: Bool) {
+        NetworkServiesMovies.shared.sycnUsers(searchTerm: "popular") {
+            self.movies = self.database.fetch(Movies.self)
+        }
+    }
+        
+//        MARK: Fetch data to Core Data
+        override func viewWillAppear(_ animated: Bool) {
+            //        let results = database.fetch(Movies.self)
+            //        print(results.map { $0.title})
+                    movies = database.fetch(Movies.self)
+        }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return .init(width: view.frame.width, height: 350)
     }
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return movies?.count ?? 0
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! PopulaViewCell
+        cell.nameLabel.text = movies?[indexPath.row].title
+        cell.dateLabel.text = movies?[indexPath.row].release_date
+        cell.ratingLabel.text = movies?[indexPath.row].vote_average.toString()
         return cell
     }
 }
---
+++ tmdMovie/Extensions/ExtDoubleToString.swift
@@ -0,0 +1,15 @@
+//
+//  ExtDoubleToString.swift
+//  tmdMovie
+//
+//  Created by Memo Figueredo on 14/9/21.
+//
+
+import Foundation
+
+
+extension Double {
+    func toString() -> String {
+        return String(format: "%.1f",self)
+    }
+}
